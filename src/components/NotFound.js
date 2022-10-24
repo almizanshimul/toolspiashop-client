c775be4d8a0162c811382d6526a51ac673ad990d
Al <almizan1040@gmail.com>
2022-10-24 23:00:14 +0600

    not found page added

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='flex items-center justify-center'>
+            <img className='w-full h-auto' src='https://i.ibb.co/rm5WX9F/notfound.jpg' alt="not found" />
+        </div>
+    );
+};
+
+export default NotFound;
