775ace8a4d284055eeba39f6d3a22f37a71d432f
Al <almizan1040@gmail.com>
2022-10-24 23:22:02 +0600

    all user ahow and make admin button

---
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useQuery } from "react-query";
+import axiosPrivate from "../../api/axiosPrivate";
+import Loading from "../../components/Loading";
+import MakeAdminRow from "./MakeAdminRow";
+
+const MakeAdmin = () => {
+  const { data: users, isLoading, refetch } = useQuery("users", () => {
+    return axiosPrivate
+      .get("https://toolspiashop.herokuapp.com/user")
+      .then((res) => res.data);
+  });
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+  return (
+    <div>
+      <h2 className="text-2xl">
+        Make A User <span className="text-primary">Admin</span>
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user, index) => <MakeAdminRow key={user._id} refetch={refetch} index={index} user={user}></MakeAdminRow>)
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default MakeAdmin;
---
+++ src/Pages/Dashboard/MakeAdminRow.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { toast } from "react-toastify";
+import axiosPrivate from "../../api/axiosPrivate";
+
+const MakeAdminRow = ({ user, index, refetch }) => {
+  const { name, email, role } = user;
+  const makeUserAdmin = () => {
+    axiosPrivate
+      .put(`https://toolspiashop.herokuapp.com/makeAdmin/${email}`, { role: 'admin' })
+      .then((res) => {
+        if (res.data.acknowledged === true) {
+          toast.success("Admin Making Successful", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+          refetch();
+        } else {
+          toast.error("Something Went Wrong", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      });
+  };
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <td>{name}</td>
+      <td>{email}</td>
+      <td>
+        {role === "admin" ? (
+          <p className="text-green-500">Admin</p>
+        ) : (
+          <button onClick={makeUserAdmin} className="btn btn-primary">Make Admin</button>
+        )}
+      </td>
+    </tr>
+  );
+};
+
+export default MakeAdminRow;
